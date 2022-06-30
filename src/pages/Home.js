3f71b3f2e59b7820c3722a778fa69fce205e01ff
njoud-almubarak <102929499+njoud-almubarak@users.noreply.github.com>
2022-06-30 11:44:31 +0300

    update context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,69 +1,48 @@
-import React,{useState,useEffect} from "react";
-import axios from "axios";
-import Header from '../component/Header'
+import React, { useState, useEffect, useContext } from "react";
+
+import Header from "../component/Header";
 import Card from "../component/Card";
-import {BiSearchAlt} from "react-icons/bi";
+import { BiSearchAlt } from "react-icons/bi";
 import Footer from "../component/Footer";
 import Aboutus from "../component/Aboutus";
-import { useNavigate } from 'react-router-dom';
-//import Loadcontext from '../context/Lodingcontext';
-//import { useContext } from 'react';
+import LoadContext from "../context/Lodingcontext";
 
 const Home = () => {
+  const { start, search, bookData, searchBook, setSearch } =
+    useContext(LoadContext);
+
+  useEffect(() => {
+    start();
+  }, []);
 
 
-    const [search,setSearch]=useState("");
-    const [bookData,setData]=useState([]);
-    const Navigate=useNavigate();
-   // const {loading} =useContext(Loadcontext);
-
-    useEffect(() => {
-        try {
-            axios.get(`https://www.googleapis.com/books/v1/volumes?q=java&maxResults=40&startIndex=1`)
-            .then(res=>setData(res.data.items))
-            .catch(err=>console.log(err))
-            } 
-         
-         catch (error) {
-            Navigate('/');
-        }
-      }, []);
-
-    const searchBook=(e)=>{
-     //   loading=true;
-        if(e.key==="Enter")
-        {
-            axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=40&startIndex=1`)
-            .then(res=>setData(res.data.items))
-            .catch(err=>console.log(err))
-        }
-    }
   return (
-   <>
-  
-   <Header/>
+    <>
+      <Header />
 
-   <div className="row2">
-                    <h6>Find Your Book</h6>
-                    <div className="search">
-                        <BiSearchAlt/>
-                        <input  type="text"  name="search" placeholder="Enter Your Book Name"
-                        value={search} onChange={e=>setSearch(e.target.value)}
-                        onKeyPress={searchBook}/>
-                       </div></div>
-   
-   
-                        <div className="container"> {
-                    <Card key={bookData.id} book={bookData}/> }    </div>
-                    <Aboutus/>
-            <Footer/>
-   
-   
-   
-   
-   </>
-  )
-}
+      <div className="row2">
+        <h6>Find Your Book</h6>
+        <div className="search">
+          <BiSearchAlt />
+          <input
+            type="text"
+            name="search"
+            placeholder="Enter Your Book Name"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={searchBook}
+          />
+        </div>
+      </div>
 
-export default Home
+      <div className="container">
+        {" "}
+        {<Card key={bookData.id} book={bookData} />}{" "}
+      </div>
+      <Aboutus />
+      <Footer />
+    </>
+  );
+};
 
+export default Home;
