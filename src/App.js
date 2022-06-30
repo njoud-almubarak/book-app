27c87f91c8f063f801a01c1cb2942d0475003a8a
njoud-almubarak <102929499+njoud-almubarak@users.noreply.github.com>
2022-06-30 11:49:10 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-//import { useContext } from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Notfound from './pages/Notfound'
-import Loading from './pages/Loading';
-//import LoadContext from './context/Lodingcontext';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Notfound from "./pages/Notfound";
+import { LoadProvider } from "./context/Lodingcontext";
 function App() {
- // const {loading} =useContext(LoadContext);
-
- // if (loading) {
-   // return <Loading />;}
-
   return (
-  //  <>
-  //  <LoadContext.Provider>
-    <BrowserRouter>
-    <Routes>
-    
-    <Route path='/' element= {<Home/>}/>
-     <Route path='/Contact' element={<Contact/>}/>
-    <Route path='*' element={<Notfound/>}/>
-    
-    
-    </Routes>
-        </BrowserRouter>
-      //  </LoadContext.Provider>
-      //  </>
+    <LoadProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
+    </LoadProvider>
   );
 }
 
