d08c8c833731ed1c2b0bbfea7fd588050b62b2e3
njoud-almubarak <102929499+njoud-almubarak@users.noreply.github.com>
2022-06-30 11:47:58 +0300

    update context

--- src/context/Lodingcontext.js
+++ src/context/Lodingcontext.js
@@ -1,16 +1,41 @@
-/*import { createContext, useState } from "react";
-const LoadContext=createContext();
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+const LoadContext = createContext();
 
+export const LoadProvider = ({ children }) => {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
 
-export const LoadProvider=({children})=>{
+  function start() {
+    try {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=java&maxResults=40&startIndex=1`
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err));
+    } catch (error) {console.log('error ');}
+  }
 
-    const[loading , setLoading]=useState(false);
+  const searchBook = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=40&startIndex=1`
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err));
+    }
+  };
 
-    return (
-        <LoadContext.Provider
-        value={{loading}}>{children}</LoadContext.Provider>
-
-    );
+  return (
+    <LoadContext.Provider
+      value={{ start, search, bookData, searchBook, setSearch }}
+    >
+      {children}
+    </LoadContext.Provider>
+  );
 };
 
-export default LoadContext;*/
+export default LoadContext;
